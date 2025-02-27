c7a0884d669d6aacd152b4a32a4365f29e8fa01c
Luca Wijsman <186515@novacollege.nl>
2025-02-27 22:01:44 +0100

    web

--- routes/web.php
+++ routes/web.php
@@ -8,19 +8,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::name("gebruiker.")->group(function(){
-    Route::get('/gebruiker/home',[GebruikerController::class,'home'])->name('home');
-    Route::get('/gebruiker/register',[GebruikerController::class,'register'])->name('register');
-    Route::post('/gebruiker/store',[GebruikerController::class,'store'])->name('store');
-    Route::get('/gebruiker/login',[GebruikerController::class,'login'])->name('login');
-    Route::get('/gebruiker/dashboard',[GebruikerController::class,'dasboard'])->name('dasboard');
-    Route::get('/gebruiker/edit/{id}',[GebruikerController::class,'edit'])->name('edit');
-    Route::post('/gebruiker/update/{id}',[GebruikerController::class,'update'])->name('update');
-    Route::post('/gebruiker/authenticate',[GebruikerController::class,'authenticate'])->name('authenticate');
+Route::name("gebruiker.")->group(function () {
+    Route::get('/gebruiker/home', [GebruikerController::class, 'home'])->name('home');
+    Route::get('/gebruiker/register', [GebruikerController::class, 'register'])->name('register');
+    Route::post('/gebruiker/store', [GebruikerController::class, 'store'])->name('store');
+    Route::get('/gebruiker/login', [GebruikerController::class, 'login'])->name('login');
+    Route::get('/gebruiker/dashboard', [GebruikerController::class, 'dashboard'])->name('dashboard');
+    Route::get('/gebruiker/edit/{id}', [GebruikerController::class, 'edit'])->name('edit');
+    Route::post('/gebruiker/update/{id}', [GebruikerController::class, 'update'])->name('update');
+    Route::post('/gebruiker/authenticate', [GebruikerController::class, 'authenticate'])->name('authenticate');
 });
 
-Route::name("events.")->group(function(){
-    Route::get('/events/home',[EvenementController::class,'home'])->name('home');
-    Route::get('/events/index',[EvenementController::class,'index'])->name('index');
-    Route::get('/events/create',[EvenementController::class,'create'])->name('create');
+Route::name("events.")->group(function () {
+    Route::get('/events/home', [EvenementController::class, 'home'])->name('home');
+    Route::get('/events/index', [EvenementController::class, 'index'])->name('index');
+    Route::get('/events/create', [EvenementController::class, 'create'])->name('create');
 });
