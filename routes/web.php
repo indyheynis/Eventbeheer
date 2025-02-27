6b7a9cbf29fb2841d7d4e0c56142ca4c1c933b20
Luca Wijsman <186515@novacollege.nl>
2025-02-27 10:23:42 +0100

    Routes toevoegen

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Http\Controllers\GebruikerController;
+use App\Http\Controllers\EvenementController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::name("gebruiker.")->group(function(){
+    Route::get('/gebruiker/home',[GebruikerController::class,'home'])->name('home');
+    Route::get('/gebruiker/login',[GebruikerController::class,'login'])->name('login');
+    Route::get('/gebruiker/register',[GebruikerController::class,'register'])->name('register');
+    Route::post('/gebruiker/store',[GebruikerController::class,'store'])->name('store');
+});
+
+Route::name("events.")->group(function(){
+    Route::get('/events/home',[EvenementController::class,'home'])->name('home');
+    Route::get('/events/index',[EvenementController::class,'index'])->name('index');
+    Route::get('/events/create',[EvenementController::class,'create'])->name('create');
 });
