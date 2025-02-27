d448178d5a37be1454e7e88325fdd7389efce423
Luca Wijsman <186515@novacollege.nl>
2025-02-27 10:25:12 +0100

    gebruikercontroller

--- app/Http/Controllers/GebruikerController.php
+++ app/Http/Controllers/GebruikerController.php
@@ -3,11 +3,41 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Gebruiker;
 
 class GebruikerController extends Controller
 {
     public function home()
     {
         return view('gebruiker.home');
+    }
+
+    public function login()
+    {
+        return view('gebruiker.login');
+    }
+
+    public function register()
+    {
+        return view('gebruiker.register');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+            'role' => 'required'
+        ]);
+
+        $gebruiker = new Gebruiker();
+        $gebruiker->name = $request->name;
+        $gebruiker->email = $request->email;
+        $gebruiker->password = $request->password;
+        $gebruiker->role = $request->role;
+        $gebruiker->save();
+
+        return redirect()->route('gebruiker.home');
     }
 }
