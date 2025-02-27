0f74e7c3bcb0b42a139c2219535cbba1a29f1dfc
Hidde Manz <hiddemanz@gmail.com>
2025-02-27 11:14:31 +0100

    dashboard aangemaakt

---
+++ resources/views/gebruiker/dashboard.blade.php
@@ -0,0 +1,23 @@
+<x-base-layout>
+    <div class="container">
+        <h1>Dashboard</h1>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Role</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($gebruikers as $gebruiker)
+                    <tr>
+                        <td>{{ $gebruiker->name }}</td>
+                        <td>{{ $gebruiker->email }}</td>
+                        <td>{{ $gebruiker->role }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</x-base-layout>
