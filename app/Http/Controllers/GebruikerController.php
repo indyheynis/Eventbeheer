24a673d2caa49f6bd04ec9f2fbd24582ae5df266
Hidde Manz <hiddemanz@gmail.com>
2025-02-27 11:12:47 +0100

    dashboard function toegevoegd

--- app/Http/Controllers/GebruikerController.php
+++ app/Http/Controllers/GebruikerController.php
@@ -12,6 +12,12 @@ class GebruikerController extends Controller
         return view('gebruiker.home');
     }
 
+    public function dasboard()
+    {
+        $gebruikers = Gebruiker::all();
+        return view('gebruiker.dasboard', compact('gebruikers'));
+    }
+
     public function login()
     {
         return view('gebruiker.login');
@@ -32,6 +38,31 @@ class GebruikerController extends Controller
         ]);
 
         $gebruiker = new Gebruiker();
+        $gebruiker->name = $request->name;
+        $gebruiker->email = $request->email;
+        $gebruiker->password = $request->password;
+        $gebruiker->role = $request->role;
+        $gebruiker->save();
+
+        return redirect()->route('gebruiker.home');
+    }
+
+    public function edit($id)
+    {
+        $gebruiker = Gebruiker::find($id);
+        return view('gebruiker.edit', compact('gebruiker'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+            'role' => 'required'
+        ]);
+
+        $gebruiker = Gebruiker::find($id);
         $gebruiker->name = $request->name;
         $gebruiker->email = $request->email;
         $gebruiker->password = $request->password;
