1bfab90d07ca463f14f24ddfd0943dffa894f559
Hidde Manz <hiddemanz@gmail.com>
2025-02-27 11:14:06 +0100

    seeder gevuld met data

--- database/seeders/GebruikerSeeder.php
+++ database/seeders/GebruikerSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,37 @@ class GebruikerSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $gebruikers = 
+        [
+            [
+                'name' => 'Hidde Manz',
+                'email' => '191319@novacollege.nl',
+                'password' => 'kaas',
+                'role' => 'manager'
+            ],
+            [
+                'name' => 'Luca Wijsman',
+                'email' => '186515@novacollege.nl',
+                'password' => 'brood',
+                'role' => 'manager'
+            ],
+            [
+                'name' => 'Indy Heijnis',
+                'email' => '192879@novacollege.nl',
+                'password' => 'boter',
+                'role' => 'employee'
+            ],
+            [
+                'name' => 'Lorenzo Schemmekes',
+                'email' => '191976@novacollege.nl',
+                'password' => 'eieren',
+                'role' => 'host'
+            ]
+
+            
+        ];
+        DB::table('gebruikers')->insert($gebruikers);        
     }
+
+
 }
