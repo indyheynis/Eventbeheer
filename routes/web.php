87c0eb5fe8bbec0cd5c9afa010cab91f1c2b6212
Hidde Manz <hiddemanz@gmail.com>
2025-02-27 11:14:42 +0100

    routes toegevoegd

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,12 @@ Route::get('/', function () {
 
 Route::name("gebruiker.")->group(function(){
     Route::get('/gebruiker/home',[GebruikerController::class,'home'])->name('home');
+    Route::get('/gebruiker/dasboard',[GebruikerController::class,'dasboard'])->name('dasboard');
     Route::get('/gebruiker/login',[GebruikerController::class,'login'])->name('login');
     Route::get('/gebruiker/register',[GebruikerController::class,'register'])->name('register');
     Route::post('/gebruiker/store',[GebruikerController::class,'store'])->name('store');
+    Route::get('/gebruiker/edit/{id}',[GebruikerController::class,'edit'])->name('edit');
+    Route::post('/gebruiker/update/{id}',[GebruikerController::class,'update'])->name('update');
 });
 
 Route::name("events.")->group(function(){
