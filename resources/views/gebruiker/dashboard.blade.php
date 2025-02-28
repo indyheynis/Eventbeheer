a11bb0354e3cbbcb319753ccbe6e46d990c57d88
Luca Wijsman <186515@novacollege.nl>
2025-02-28 09:17:42 +0100

    changed layout

--- resources/views/gebruiker/dashboard.blade.php
+++ resources/views/gebruiker/dashboard.blade.php
@@ -1,23 +1,30 @@
 <x-base-layout>
+    <x-slot name="title">Dashboard</x-slot>
+    <x-slot name="styles">
+        <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
+    </x-slot>
+
     <div class="container">
         <h1>Dashboard</h1>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Role</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($gebruikers as $gebruiker)
+        <div class="table-responsive">
+            <table class="table table-striped table-bordered">
+                <thead class="thead-dark">
                     <tr>
-                        <td>{{ $gebruiker->name }}</td>
-                        <td>{{ $gebruiker->email }}</td>
-                        <td>{{ $gebruiker->role }}</td>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Role</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @foreach($gebruikers as $gebruiker)
+                        <tr>
+                            <td>{{ $gebruiker->name }}</td>
+                            <td>{{ $gebruiker->email }}</td>
+                            <td>{{ $gebruiker->role }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 </x-base-layout>
