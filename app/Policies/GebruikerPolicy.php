b84cb26f1b24c41e744d961ba63b0388c39a5b3d
Hidde Manz <hiddemanz@gmail.com>
2025-03-10 13:30:06 +0100

    gebruiker policy aangemaakt en gevuld

---
+++ app/Policies/GebruikerPolicy.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+
+class GebruikerPolicy
+{
+    /**
+     * Create a new policy instance.
+     */
+    // public function __construct()
+    // {
+       
+    // }
+
+    // public function viewAny(User $user)
+    // {
+    //     return in_array($user->role, ['manager', 'employee']);
+    // }
+
+    // public function edit(User $user)
+    // {
+    //     return in_array($user->role, ['manager', 'employee']);
+    // }
+
+    // public function delete(User $user)
+    // {
+    //     return in_array($user->role, ['manager', 'employee']);
+    // }
+}
