b79be7505920449790ce2e89c2363323ce7c33e2
Luca Wijsman <186515@novacollege.nl>
2025-03-18 10:29:04 +0100

    added event and ticket

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Ticket;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -22,6 +23,8 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             GebruikerSeeder::class,
+            EvenementSeeder::class,
+            TicketSeeder::class,
         ]);
     }
 }
